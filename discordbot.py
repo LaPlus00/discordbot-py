ddb12acf5d51fcd20bf248d79980472a4a7663f4
LaPlus00 <129968113+LaPlus00@users.noreply.github.com>
2023-04-05 23:38:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -27,7 +27,7 @@ async def on_message(message):
         await message.channel.send("callback!")
 
         if message.content.startswith(f'{PREFIX} 도움말'):
-        embed = discord.Embed(
+            embed = discord.Embed(
             title = '명령어 목록',
             description = '아직 공부하는 중이에요! 더 완벽해질 수 있도록 노력할게요!.',
             colour = discord.Colour.blue()
